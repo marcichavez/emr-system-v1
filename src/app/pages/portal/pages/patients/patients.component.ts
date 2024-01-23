805e2e3c9c13d4c24a7e9d02f2fa3c6fa9359b55
marcichavez <marci@lgusuite.com>
2024-01-23 17:51:58 +0800

    added patient masterlist

--- src/app/pages/portal/pages/patients/patients.component.ts
+++ src/app/pages/portal/pages/patients/patients.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { MOCK_PATIENT_LIST } from './patient.mock';
 
 @Component({
   selector: 'app-patients',
   templateUrl: './patients.component.html',
-  styleUrls: ['./patients.component.scss']
+  styleUrls: ['./patients.component.scss'],
 })
 export class PatientsComponent implements OnInit {
+  keyword = new FormControl('');
 
-  constructor() { }
+  displayedColumns: string[] = [
+    'pin',
+    'memberPin',
+    'sex',
+    'lastName',
+    'firstName',
+    'middleName',
+    'suffix',
+    'dob',
+    'age',
+    'mobileNo',
+    'memberType',
+    'memberName',
+    'memberDob',
+  ];
+  dataSource = MOCK_PATIENT_LIST;
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
--- src/app/pages/portal/pages/patients/patients.module.ts
+++ src/app/pages/portal/pages/patients/patients.module.ts
@@ -3,15 +3,31 @@ import { CommonModule } from '@angular/common';
 
 import { PatientsRoutingModule } from './patients-routing.module';
 import { PatientsComponent } from './patients.component';
-
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [
-    PatientsComponent
-  ],
+  declarations: [PatientsComponent],
   imports: [
     CommonModule,
-    PatientsRoutingModule
-  ]
+    PatientsRoutingModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatTableModule,
+    MatInputModule,
+    MatSelectModule,
+    FormsModule,
+    MatIconModule,
+    ReactiveFormsModule,
+    MatTooltipModule,
+    MatButtonModule,
+  ],
 })
-export class PatientsModule { }
+export class PatientsModule {}
