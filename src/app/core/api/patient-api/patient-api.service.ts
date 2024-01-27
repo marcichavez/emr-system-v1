171420d2ed0404cf339556f1b58925cf42fc2afe
Marci <marci@lgusuite.com>
2024-01-27 14:16:50 +0800

    patient api wip

--- src/app/core/api/patient-api/patient-api.service.ts
+++ src/app/core/api/patient-api/patient-api.service.ts
@@ -1,9 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PatientApiService {
-
-  constructor() { }
+  constructor(private httpClient: HttpClient) {}
 }
