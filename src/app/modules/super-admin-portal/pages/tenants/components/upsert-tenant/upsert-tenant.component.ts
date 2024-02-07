f9f659d1d0ce85f867b762bccd246837e0fc824f
fmikko <francismikko.antonio@lgusuite.com>
2024-02-08 02:28:17 +0800

    Add Upsert Tenant

---
+++ src/app/modules/super-admin-portal/pages/tenants/components/upsert-tenant/upsert-tenant.component.ts
@@ -0,0 +1,116 @@
+import { Component, Inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { UserService } from '@core/api/user/user.service';
+import { STATUS } from '@core/constants/STATUS.constant';
+import { User } from '@core/interfaces/models/User.interface';
+import { ConfirmationDialogService } from '@shared/components/confirmation-dialog/confirmation-dialog.service';
+import { SnackbarService } from '@shared/components/snackbar/snackbar.service';
+import { filter, switchMap } from 'rxjs/operators';
+import { UPSERT_TENANT_CONFIG } from './upsert-tenant.config';
+
+export interface UpsertTenantConfig {
+  title: string;
+  confirmationDialog: {
+    title: string;
+    message: string;
+    cancelText?: string;
+    confirmText?: string;
+  };
+}
+
+@Component({
+  selector: 'app-upsert-tenant',
+  templateUrl: './upsert-tenant.component.html',
+  styleUrls: ['./upsert-tenant.component.scss'],
+})
+export class UpsertTenantComponent {
+  tenantFormGroup: FormGroup;
+  serverErrorMessage = '';
+  config: UpsertTenantConfig;
+  isUpdate: boolean;
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public tenant: User,
+    private formBuilder: FormBuilder,
+    private userService: UserService,
+    private confirmationDialogService: ConfirmationDialogService,
+    private snackbarService: SnackbarService,
+    private dialogRef: MatDialogRef<UpsertTenantComponent>,
+  ) {
+    this.isUpdate = !!this.tenant;
+    this.config = this.isUpdate
+      ? UPSERT_TENANT_CONFIG.UPDATE
+      : UPSERT_TENANT_CONFIG.CREATE;
+
+    this.tenantFormGroup = this.formBuilder.group({
+      firstName: ['', []],
+      lastName: ['', []],
+      occupation: ['', []],
+      licenseNumber: [''],
+      email: ['', [, Validators.email]],
+    });
+
+    if (this.isUpdate) {
+      this.tenantFormGroup.reset(this.tenant);
+    }
+  }
+
+  onSaveTenant() {
+    this.confirmationDialogService
+      .openConfirmationDialog(this.config.confirmationDialog)
+      .pipe(
+        filter((confirm) => confirm),
+        switchMap(() => {
+          const body = this.tenantFormGroup.getRawValue();
+          this.snackbarService.openLoadingSnackbar('Saving tenant...');
+
+          if (this.isUpdate) {
+            return this.userService.updateUser(body, this.tenant._id);
+          }
+
+          return this.userService.createUser(body);
+        }),
+      )
+      .subscribe(
+        () => {
+          this.snackbarService.openSuccessSnackbar('Tenant saved successfully');
+          this.dialogRef.close(true);
+        },
+        (err) => {
+          this.snackbarService.openErrorSnackbar(
+            'Failed to save tenant, please check the form',
+          );
+          this.serverErrorMessage = err.error.message;
+        },
+      );
+  }
+
+  onDeleteTenant() {
+    this.confirmationDialogService
+      .openConfirmationDialog({
+        title: 'Delete Tenant Confirmation',
+        message: 'Are you sure you want to delete this tenant?',
+      })
+      .pipe(
+        filter((confirm) => confirm),
+        switchMap(() => {
+          this.snackbarService.openLoadingSnackbar('Deleting tenant...');
+
+          return this.userService.patchUserStatus(
+            STATUS.DELETED,
+            this.tenant._id,
+          );
+        }),
+      )
+      .subscribe(
+        () => {
+          this.snackbarService.openSuccessSnackbar('Tenant has been deleted');
+          this.dialogRef.close(true);
+        },
+        (err) => {
+          this.serverErrorMessage = err.error.message;
+        },
+      );
+  }
+}
