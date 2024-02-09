2b9552e5aaadb53f23ea2ce8fb9c16d0945575c3
fmikko <francismikko.antonio@lgusuite.com>
2024-02-09 14:16:37 +0800

    Add Employee Routing and navigation

--- src/app/modules/portal/portal-routing.module.ts
+++ src/app/modules/portal/portal-routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '@modules/auth/guards/auth.guard';
 import { PortalComponent } from './portal.component';
 
 const routes: Routes = [
   {
     path: '',
     component: PortalComponent,
+    canActivate: [AuthGuard],
     children: [
+      {
+        path: '',
+        redirectTo: 'patients-masterlist',
+        pathMatch: 'full',
+      },
       {
         path: 'patients-masterlist',
         loadChildren: () =>
@@ -19,6 +26,13 @@ const routes: Routes = [
         loadChildren: () =>
           import('./pages/appointments/appointments.module').then(
             (m) => m.AppointmentsModule,
+          ),
+      },
+      {
+        path: 'employees',
+        loadChildren: () =>
+          import('./pages/employees/employees.module').then(
+            (m) => m.EmployeesModule,
           ),
       },
     ],
