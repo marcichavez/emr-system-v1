dd46123177ae34e8399d9c06e0faf899f85b4e12
fmikko <francismikko.antonio@lgusuite.com>
2024-02-09 14:19:21 +0800

    Bug fix portal routing auth guard

--- src/app/modules/portal/portal-routing.module.ts
+++ src/app/modules/portal/portal-routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthGuard } from '@modules/auth/guards/auth.guard';
 import { PortalComponent } from './portal.component';
 
 const routes: Routes = [
   {
     path: '',
     component: PortalComponent,
-    canActivate: [AuthGuard],
     children: [
       {
         path: '',
